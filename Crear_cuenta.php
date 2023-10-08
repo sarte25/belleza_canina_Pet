bbd094ec638ed44e0f872c6a6216be34511c1166
sarte25 <sebasmena.121@gmail.com>
2023-10-08 17:32:43 -0500

    Primera subida del archivo crear cuenta el cual se usa para mostrar el formulario y enviar la información al archivo grabar php necesaria para hacer el registro si así lo desean los clientes para contactar con ellos por medio de Gmail

---
+++ Crear_cuenta.php
@@ -0,0 +1,84 @@
+<html>
+   <head>
+   <meta charset="UTF-8">
+    <meta name="description" content="belleza canina ">
+    <title> PET SHOP Y BELLEZA CANINA ESTRELLITA </title>
+    <link rel="stylesheet" href="normalice.2.css">
+    <link rel="stylesheet" href="style1.css">
+    <link rel="shorcut icon"href="http://drive.google.com/uc?export=view&id=1sirRNx3BVz4HX9gp0AhP8lNk6_fL0UNT">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@500&display=swap" rel="stylesheet">
+    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+   </head>  
+   </head> 
+   <body>
+    <header>
+        <div>  
+               <nav class="barra">
+                  <center><h1> BELLEZA CANINA ESTRELLITA</h1></center>
+               <img src="http://drive.google.com/uc?export=view&id=1t2pNB5Cnz-do-z9dq6GdNJk1AmI1-hsJ" width="180" height="180">                
+                    <div class="menu"> 
+                      <ul>
+                        <li><a href="bellesa_canina.html">Inicio<i class='bx bxs-home bx-tada-hover' ></i></a></li>
+                        <li><a href="servicios.html">Servicios <i class='bx bxs-down-arrow bx-tada-hover'></i></a>
+                        <ul>
+                          <li><a href="perro.html" >Perros <i class='bx bxs-dog bx-tada-hover'></i> </a></li>
+                          <li><a href="gato.html" >Gatos <i class='bx bxs-cat bx-tada-hover'></i></a></li>
+                        </ul>
+                        </li>
+                        <li><a href="contacto.html">Contacto</a></li>
+                        <li> <a href="ayuda.html">Ayuda <i class='bx bx-help-circle bx-tada-hover'></i></a> </li> 
+                       
+                       </div>  
+                     </ul>
+   
+                     <ul class="ul2">
+                    <li><a href="Crear_cuenta.php">Registrarse<i class='bx bxs-user-account bx-tada-hover'></i></a>
+                     </li>
+                  </ul>
+                </nav>
+        </div>   
+        
+    </header> 
+    
+    <section class="form-register">
+    <form action="grabar.php"method="post">
+      <h4><center>REGISTRARSE</center></h4>
+       <input class="controls" type="text" name="wnombre" placeholder="Ingrese su Nombre" required>
+       <input class="controls" type="text" name="wapellido"  placeholder="Ingrese su Apellido" required>
+       <input class="controls" type="email" name="wcorreo" placeholder="Ingrese su Correo" required>
+       <input class="controls" type="text" name="wbarrio" placeholder="Ingrese su Barrio">
+       <input class="controls" type="text" name="wrecidencia"  placeholder="Ingrese su Lugar de Recidencia" required>
+     </font>  
+      <p>Estoy de acuerdo con <a href="politica_privacidad.html">Terminos y Condiciones</a></p>
+      <a href="bellesa_canina.html"><input class="botons" type="submit" value="Registrar"></a>
+      <p><a href="bellesa_canina.html">¿Ya estoy registrado?</a></p>
+    
+    </section> 
+           <!-- <div class="container2"> </div> NO borrar, sirve para la margen-->
+          </br></br></br></br></br>
+
+              <footer class="foother">
+              <div class="container_foother">
+                <section class="descargas">
+                </section>
+                <section class="email">
+               <div class="socialmedia">
+                <p class="socialmedia_legend">Síguenos en: </p>
+                <a class="redes" href="https://www.facebook.com/sergioandres.moralesmartinez.7?mibextid=ZbWKwL" target="_blank" style="text-decoration: none;"><i class='socialmedia_icon bx bxl-facebook-circle'></i></a>
+                <a href="http://Instagram.com/sergio_4585/" target="_blank"><i class='socialmedia_icon bx bxl-instagram'></i></a>
+                <a href="https://twitter.com/segioandres12?t=W4LqkoC5C1cUb4sfL-UHMA&s=09" target="_blank"><i class='socialmedia_icon bx bxl-twitter'></i></a>
+
+               </div>
+                </section>
+              <div class="foother_copyright">
+               <p class="copyrigth">PET SHOP Y BELLEZA CANINA &copy; 2022 Todos los derechos reservados</p></div>
+               <a href="politica_privacidad.html" class="politica__privacidad--marggin">Política de privacidad</a>
+               <a href="politica_privacidad.html" class="politicadeprivacidad">Términos y condiciones</a>
+              </div>
+              <img class="imagen" src="http://drive.google.com/uc?export=view&id=1t2pNB5Cnz-do-z9dq6GdNJk1AmI1-hsJ" width="280" height="280">                
+
+            </footer>    
+  </body>
+</html>
